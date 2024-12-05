192b75faef888a381731b198e265b6270308a188
pawan raju <pawankohli438@gmail.com>
2024-12-06 00:03:20 +0530

    fixed dowload resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -56,7 +56,7 @@ const Hero = () => {
             View Projects
           </a>
           <a
-            href="/Pawan Raju new fullstack.pdf" // Update to a relative path
+            href="Pawan Raju new fullstack.pdf" // Update to a relative path
             download
             className="border text-primary border-accent text-accent hover:bg-accent/10 p-2 sm:px-6 sm:py-3 rounded-lg transition-all transform hover:scale-105 hover:shadow-lg"
           >
