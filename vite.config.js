52939d88be888904ffacd72dd936de9e327c5dd8
pawan raju <pawankohli438@gmail.com>
2024-12-05 23:45:07 +0530

    vite file update path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Root base path for Netlify
+  base: '/my-portfolio/', // Root base path for Netlify
 })
